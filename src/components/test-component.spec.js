c3289c5b89cb19d40128eb89de932c752c653fca
Marcos Germano <marcos.germano@b2wdigital.com>
2021-03-17 23:27:28 -0300

    removing test snapshot

--- src/components/test-component.spec.js
+++ src/components/test-component.spec.js
@@ -6,9 +6,6 @@ const title = 'Test Title';
 let wrapped = shallow(<Title>{title}</Title>);
 
 describe('Title', () => {
-  it('should render the Title Component correctly', () => {   
-    expect(wrapped).toMatchSnapshot();
-  });
   it('renders the Titles children', () => { 
     expect(wrapped.find('h1').text()).toEqual(title);
   });
