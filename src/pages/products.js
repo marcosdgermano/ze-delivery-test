dd2637ded10ef9604cd922aa3d85fca3614173f3
Marcos Germano <marcos.germano@b2wdigital.com>
2021-03-20 19:10:34 -0300

    creating basic header

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import ProductsList from '../components/product-list';
 
-const Products = () => <ProductsList />;
+const Products = () => (
+  <PageContainer>
+    <ProductsList />
+  </PageContainer>
+);
+
+const PageContainer = styled.div`
+  margin-top: 135px;
+`;
 
 export default Products;
