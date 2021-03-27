4de2245edddb1e596d092fa4cb1d9d86c8b77730
Marcos Germano <marcos.germano@b2wdigital.com>
2021-03-27 19:06:38 -0300

    creating redirect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import { client as apolloClient } from './utils/graphql-client';
 import GlobalStyle from './utils/global-style';
@@ -10,25 +10,34 @@ import Header from './components/header';
 import Footer from './components/footer';
 import Home from './pages/home';
 import Products from './pages/products';
+import { getCookie } from './utils/helpers';
 
 const Page = styled.div`
   min-height: 70vh;
 `;
 
-const App = () => (
-  <ApolloProvider client={apolloClient}>
-    <GlobalStyle />
-    <Router history={history}>
-      <Header />
-      <Page>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/produtos" component={Products} />
-        </Switch>
-      </Page>
-      <Footer />
-    </Router>
-  </ApolloProvider>
-);
+const App = () => {
+  const hasPocId = !!getCookie('pocId');
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <GlobalStyle />
+      <Router history={history}>
+        <Header />
+        <Page>
+          <Switch>
+            <Route path="/" exact>
+              {hasPocId ? <Redirect to="/produtos" /> : <Home />}
+            </Route>
+            <Route path="/produtos" exact>
+              {hasPocId ? <Products /> : <Redirect to="/" />}
+            </Route>
+          </Switch>
+        </Page>
+        <Footer />
+      </Router>
+    </ApolloProvider>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
