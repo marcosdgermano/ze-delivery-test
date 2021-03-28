612d2ad42887539471c33cc52cc88fa9fe2e59b8
Marcos Germano <marcos.germano@b2wdigital.com>
2021-03-28 17:29:48 -0300

    testing product card

--- src/pages/products/components/__tests__/product-card.spec.js
+++ src/pages/products/components/__tests__/product-card.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import ProductCard from '../product-card';
 
 describe('<ProductCard />', () => {
@@ -9,11 +9,21 @@ describe('<ProductCard />', () => {
     price: '5.49',
   };
 
-  it('Should render the component correctly', () => {
+  it('Render component', () => {
     const wrapper = shallow(<ProductCard {...props} />);
 
     expect(wrapper.find('Name').text()).toEqual('Brahma');
     expect(wrapper.find('Price').text()).toEqual('R$ 5,49');
     expect(wrapper.find('Image').props().src).toEqual('mock/testurl.jpg');
+  });
+
+  it('Update cart', () => {
+    const wrapper = mount(<ProductCard {...props} />);
+
+    wrapper.find('AddButton').simulate('click');
+    expect(wrapper.find('AddButton').props().selected).toBe(true);
+
+    wrapper.find('AddButton').simulate('click');
+    expect(wrapper.find('AddButton').props().selected).toBe(false);
   });
 });
