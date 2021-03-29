5c00d7b718d418dd5676600261faa8ea234a39fa
Marcos Germano <marcos.germano@b2wdigital.com>
2021-03-28 22:55:31 -0300

    testing home

---
+++ src/pages/home/index.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Home from './index';
+
+describe('<Home />', () => {
+  it('Render component', () => {
+    const wrapper = mount(<Home history={{ push: jest.fn() }} pocId={1} />);
+
+    expect(wrapper.isEmptyRender()).toBe(false);
+  });
+
+  it('Render error', () => {
+    const wrapper = mount(<Home error={{ message: 'Error' }} />);
+
+    expect(wrapper.find('span').text()).toEqual('error');
+  });
+
+  it('Render loading', () => {
+    const wrapper = mount(<Home loading />);
+
+    expect(wrapper.find('Loading')).toHaveLength(1);
+  });
+});
